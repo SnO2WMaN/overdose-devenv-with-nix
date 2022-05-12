786bb0c0422bb59da09f9698c1b78a5bfb462391
SnO2WMaN <me@sno2wman.net>
2022-05-12 10:00:29 +0900

    auto generated files!

--- flake.nix
+++ flake.nix
@@ -14,13 +14,13 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     devshell-files = {
-      url = "github:SnO2WMaN/devshell-files";
+      url = "github:SnO2WMaN/devshell-files/output-modules";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.devshell.follows = "devshell";
     };
     rust-tools = {
-      url = "path:./overlays/rust-tools";
+      url = "path:./flakes/rust-tools";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -38,8 +38,54 @@
         in
         pkgs.devshell.mkShell {
           imports = [
-            (pkgs.devshell.importTOML ./devshell.toml)
+            (devshell-files.devshellModules)
           ];
+          devshell.name = "Overdose";
+          devshell.packages = with pkgs;[
+            treefmt
+            nixpkgs-fmt
+            taplo-cli
+          ];
+
+          files.direnv.enable = true;
+          files.gitignore = {
+            enable = true;
+            pattern = {
+              "**/.direnv" = true;
+              ".vscode/settings.json" = true;
+              "treefmt.toml" = true;
+            };
+          };
+
+          files.json.".vscode/settings.json" =
+            let langs = [ "toml" "json" "jsonc" "nix" ]; in
+            {
+              "customLocalFormatters.formatters" = [
+                { "command" = "${pkgs.treefmt}/bin/treefmt -q --stdin \${file}"; "languages" = langs; }
+              ];
+            } //
+            builtins.listToAttrs (
+              builtins.map
+                (lang: {
+                  name = "[${lang}]";
+                  value = { "editor.defaultFormatter" = "jkillian.custom-local-formatters"; };
+                })
+                langs
+            );
+
+          files.toml."treefmt.toml" = {
+            formatter = {
+              toml = {
+                command = "${pkgs.taplo-cli}/bin/taplo";
+                includes = [ "*.toml" ];
+              };
+              nix = {
+                command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+                includes = [ "*.nix" ];
+              };
+            };
+          };
+
         };
     });
 }
